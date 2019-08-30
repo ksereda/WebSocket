73157f417db574c43330ba60b5f1916e44bd541d
ksereda <kirill.serada@gmail.com>
2019-08-30 17:02:17 +0300

    added comments to config file

--- chat-service/src/main/java/com/example/chatservice/config/WebSocketConfig.java
+++ chat-service/src/main/java/com/example/chatservice/config/WebSocketConfig.java
@@ -6,19 +6,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
-@EnableWebSocketMessageBroker
+@EnableWebSocketMessageBroker  // enable WebSocket server
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    // registering a websocket endpoint that the clients will use to connect to websocket server
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").withSockJS();  // SockJS is used to enable fallback options for browsers that don’t support websocket
     }
 
+    // configuring a message broker that will be used to route messages from one client to another
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
+        registry.setApplicationDestinationPrefixes("/app");  // the messages whose destination starts with “/app” should be routed to message-handling methods
+        registry.enableSimpleBroker("/topic");  // the messages whose destination starts with “/topic” should be routed to the message broker
+                                                                 // Message broker broadcasts messages to all the connected clients who are subscribed to a particular topic
     }
 }
