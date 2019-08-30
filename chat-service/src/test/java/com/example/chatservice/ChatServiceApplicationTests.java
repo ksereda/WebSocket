8be299da28c12d82ed384201d42c48a234c93c45
ksereda <kirill.serada@gmail.com>
2019-08-30 16:58:00 +0300

    added config class for project

--- chat-service/src/test/java/com/example/chatservice/ChatServiceApplicationTests.java
+++ chat-service/src/test/java/com/example/chatservice/ChatServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.query_performance;
+package com.example.chatservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class QueryPerformanceApplicationTests {
+public class ChatServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
